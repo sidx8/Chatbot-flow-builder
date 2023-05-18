1d9724d119248a7b5a1756b49935cb5471b13799
siddharth <siddharth0sinha@gmail.com>
2023-05-19 00:39:35 +0530

    add Textnode complete

--- app/component/TextNode.js
+++ app/component/TextNode.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Handle, Position } from "reactflow";
 
-function TextNode({ id, data, selected }) {
+//custome node
+function TextNode({ data, selected }) {
   return (
     <div
       className={`w-40  shadow-md rounded-md bg-white   ${
@@ -14,20 +15,22 @@ function TextNode({ id, data, selected }) {
         </div>
         <div className="px-3 py-2 ">
           <div className="text-xs font-normal text-black">
-            {data.name ?? "Text Node"}
+            {data.label ?? "Text Node"}
           </div>
         </div>
       </div>
 
       <Handle
+        id="a"
         type="target"
         position={Position.Left}
-        className="w-1 rounded-full bg-slate-800"
+        className="w-1 rounded-full bg-slate-500"
       />
       <Handle
+        id="b"
         type="source"
         position={Position.Right}
-        className="w-1 rounded-full bg-gray-800"
+        className="w-1 rounded-full bg-gray-500"
       />
     </div>
   );
